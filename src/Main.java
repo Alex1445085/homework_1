ee92e6c968eaa1b9913db50500f0563e59cb56a3
Alex1445085 <n1445085@yandex.ru>
2024-05-12 22:34:35 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        for (int i = 5; i >= 0; i--) {
+            System.out.print("i = " + i);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
